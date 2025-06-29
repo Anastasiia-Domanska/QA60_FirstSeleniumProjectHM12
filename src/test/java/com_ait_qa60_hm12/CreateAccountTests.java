324eaa8aa8780ef0d46c2878d87edb004c97de8a
Anastasiia-Domanska <akvadelf24@gmail.com>
2025-06-29 22:37:12 +0200

    add homework15: classes CreateAccountTests, LoginTests, TestBase

---
+++ src/test/java/com_ait_qa60_hm12/CreateAccountTests.java
@@ -0,0 +1,23 @@
+package com_ait_qa60_hm12;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateAccountTests extends TestBase{
+
+    @Test()
+    public void newUserRegistrationPositiveTest(){
+        click(By.cssSelector("[href='/register']"));
+        //click(By.cssSelector("//input[@value='Register']"));
+        click(By.id("gender-female"));
+        type(By.id("FirstName"), "Anastasiia");
+        type(By.id("LastName"), "Do");
+        type(By.id("Email"), "anastasiia23@gmail.com");
+        type(By.name("Password"), "Aa12345!");
+        type(By.id("ConfirmPassword"), "Aa12345!");
+        click(By.id("register-button"));
+        Assert.assertTrue(isElementPresent(By.cssSelector(".result")));
+        Assert.assertTrue(isElementPresent(By.xpath("//button[.='Log Out']")));
+    }
+}
